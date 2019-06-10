529750b66dc549e3495e5ef31aadd3d6eaf6b4cc
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2019-06-10 17:41:29 +0300

    extracting data

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -13,15 +13,26 @@ const getters = {};
 const actions = {
   getAllProducts({ commit }) {
     shop.getProducts((products, productsNames) => {
-      commit('setProducts', products);
+      const productsData = products.Value.Goods;
+      let goods;
+      for (let item in productsNames) {
+        console.log('outer');
+        console.log(productsNames[item]);
+        for (let innerItem in productsNames[item]) {
+          console.log('inner');
+          console.log(productsNames[item][innerItem]);
+        }
+      }
+      commit('setProducts', goods);
     });
   }
 };
 
 // mutations
 const mutations = {
-  setProducts(state, products) {
-    state.all = products;
+  setProducts(state, goods) {
+    state.all = goods;
+    console.log(state.all);
   }
 };
 
