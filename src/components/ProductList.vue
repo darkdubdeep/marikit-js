fc008c35dbd251770d9277c4cc9c785b352d4242
Ильнур Ибрагимов <ilnuribragimov@Mac-mini-Ilnur.Dlink>
2019-06-15 10:04:20 +0300

    Develop item list

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
-    <v-layout text-xs-center wrap>
-      <v-flex xs12 sm6 offset-sm3>
+    <v-layout text-xs-center justify-center wrap>
+      <v-flex xs12 sm11>
         <v-card>
           <v-toolbar color="teal" dark>
             <v-toolbar-title>Товары</v-toolbar-title>
@@ -21,20 +21,21 @@
                 </v-list-tile>
               </template>
 
-              <v-list-tile v-for="subItem in item.B" :key="subItem.id">
+              <v-list-tile v-for="subItem in item.B" :key="subItem.id" class="highed-price">
                 <v-list-tile-content>
                   <v-list-tile-title v-html="subItem.name + ' (' + subItem.quantity + ')'"></v-list-tile-title>
-                  <v-list-tile-sub-title v-html="subItem.name + ' (' + subItem.quantity + ')'"></v-list-tile-sub-title>
                 </v-list-tile-content>
+                <v-list-tile-action>
+                  <v-layout row wrap align-center>
+                    <v-list-tile-action-text
+                      class="pr-3 pb-2 title font-weight-bold"
+                    >{{ subItem.price }} р.</v-list-tile-action-text>
+                    <v-btn icon ripple>
+                      <v-icon color="teal lighten-2">shopping_cart</v-icon>
+                    </v-btn>
+                  </v-layout>
+                </v-list-tile-action>
               </v-list-tile>
-
-              <!-- <v-card v-for="subItem in item.B" :key="subItem.id">
-                <v-card-title primary-title>
-                  <div>
-                    <div>{{ subItem.name }}</div>
-                  </div>
-                </v-card-title>
-              </v-card>-->
             </v-list-group>
           </v-list>
         </v-card>
@@ -60,5 +61,9 @@ export default {
 <style>
 .v-list__group__items--no-action .v-list__tile {
   padding-left: 35px;
+}
+
+.highed-price {
+  background: #ffe0b2;
 }
 </style>
